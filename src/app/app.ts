7c6821617efc138fbb3323242ce5bf370f36b55e
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-10-10 20:49:46 +0800

    add test message

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('github actions');
+  test = signal('this is my direct change to main!');
 }
