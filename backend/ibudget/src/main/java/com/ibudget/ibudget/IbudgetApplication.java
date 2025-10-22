d989c17d2a920e8f47b0f66ab55a7215a5f0eb4d
ynion-mabeamae <mabeamaeynion@gmail.com>
2025-10-23 02:37:03 +0800

    feat: add backend directory with spring boot setup

---
+++ backend/ibudget/src/main/java/com/ibudget/ibudget/IbudgetApplication.java
@@ -0,0 +1,13 @@
+package com.ibudget.ibudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbudgetApplication.class, args);
+	}
+
+}
